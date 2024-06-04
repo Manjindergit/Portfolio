24e07d0d7a9e54bb90f2c4018f560dfec4412110
Manjindergit <30274853+Manjindergit@users.noreply.github.com>
2024-06-04 00:06:59 -0700

    background pic, profile pic fixed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import profilePic from './media/profile.jpeg';
+import background from './media/1.jpeg';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const profile = document.getElementById('profilePic');
+    profile.src = profilePic;
+
+    const mainContainer = document.getElementById('mainContainer');
+    mainContainer.style.backgroundImage = `url(${background})`;
+    //background iamge should cover entire mainContainer
+    mainContainer.style.backgroundSize = 'cover';
+}
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            }
+          },
+          "sass-loader",
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
     ],
-    
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
